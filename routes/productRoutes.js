b14f3234b1a2f6ee55b7f362d2dbd57806f67403
Your Name <you@example.com>
2026-06-15 21:30:13 +0530

    Changed the POST method to PUT for DB updates

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
-import {getproducts , addproduct} from "../controllers/productcontroller.js"
+import {getproducts , addproduct, updateprice, deleteproduct} from "../controllers/productcontroller.js"
 import { protect } from "../middlewares/authmiddleware.js";
 import { admin } from "../middlewares/adminmiddleware.js";
 import { asyncHandler } from "../utils/asynchandler.js";
 import { validate } from "../middlewares/validate.js";
-import { addproductSchema } from "../validator/productvalidator.js";
+import { addproductSchema, deleteItemSchema, updatePriceSchema } from "../validator/productvalidator.js";
 
 const router = express.Router();
 
-router.get("/info",validate(addproductSchema),asyncHandler(getproducts));
-router.post("/add",protect,admin,asyncHandler(addproduct));
+router.get("/info",asyncHandler(getproducts));
+router.post("/add",protect,admin,validate(addproductSchema),asyncHandler(addproduct));
+router.put("/update",protect,admin,validate(updatePriceSchema),asyncHandler(updateprice));
+router.post("/delete",protect,admin.validate(deleteItemSchema),asyncHandler(deleteproduct));
 
 export default router;
